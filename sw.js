698291e7d29c7f93b9ad0133be72ec5c392e628a
baydina <13266264+baydina@users.noreply.github.com>
2023-04-08 00:14:22 -0500

    pwa

--- sw.js
+++ sw.js
@@ -1,31 +1,26 @@
-const CACHE_NAME = "unit-converter-cache-v1";
-const urlsToCache = ["/", "index.html", "main.js", "styles.css"];
+var cacheName = 'unit-convert';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/js/main.js'
+];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
-
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
-  );
-});
-
-let deferredPrompt;
-
-window.addEventListener("beforeinstallprompt", (event) => {
-  event.preventDefault();
-  deferredPrompt = event;
-
-  // Show a button to prompt the user to install the PWA
-  const installButton = document.getElementById("install-button");
-  installButton.style.display = "block";
-  installButton.addEventListener("click", () => {
-    installButton.style.display = "none";
-    deferredPrompt.prompt();
-  });
-});
-
-
